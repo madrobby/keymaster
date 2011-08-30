a1477f16cb0c1a2609555647493acace6e63f82d
Thomas Fuchs <thomas@fesch.at>
2011-08-30 09:41:26 -0400

    update demo to latest keymaster version (fixes FF error)

--- keymaster.js
+++ keymaster.js
@@ -7,86 +7,117 @@
     _handlers = {}, 
     _mods = { 16: false, 18: false, 17: false, 91: false },
     _scope = 'all',
+    // modifier keys
     _MODIFIERS = {
-      shift: 16, 
+      '⇧': 16, shift: 16, 
       option: 18, '⌥': 18, alt: 18, 
-      ctrl: 17, control: 17, 
-      command: 91, '⌘': 91 },
+      ctrl: 17, control: 17,
+      command: 91, '⌘': 91
+    },
+    // special keys 
     _MAP = {
-      backspace: 8, tab: 9,
+      backspace: 8, tab: 9, clear: 12,
       enter: 13, 'return': 13,
-      escape: 27, space: 32,
+      esc: 27, escape: 27, space: 32,
       left: 37, up: 38,
-      right: 39, down: 40 };
-  
+      right: 39, down: 40,
+      del: 46, 'delete': 46,
+      home: 36, end: 35,
+      pageup: 33, pagedown: 34 };
+
+  for(k=1;k<20;k++) _MODIFIERS['f'+k] = 111+k;
+
+  // handle keydown event
   function dispatch(event){
-    var key, tagName, handler, k,i, hl, hmi, hmil, modifiersMatch;
+    var key, tagName, handler, k, i, modifiersMatch;
     tagName = (event.target || event.srcElement).tagName;
     key = event.keyCode;
+    
+    // if a modifier key, set the key.<modifierkeyname> property to true and return
+    if(key == 93 || key == 224) key = 91; // right command on webkit, command on Gecko
     if(key in _mods) {
       _mods[key] = true;
+      // 'assignKey' from inside this closure is exported to window.key
       for(k in _MODIFIERS) if(_MODIFIERS[k] == key) assignKey[k] = true;
       return;
     }
+
+    // ignore keypressed in any elements that support keyboard data input
     if (tagName == 'INPUT' || tagName == 'SELECT' || tagName == 'TEXTAREA') return;
+
+    // abort if no potentially matching shortcuts found
     if (!(key in _handlers)) return;
-    for (i = 0, hl = _handlers[key].length; i < hl; i++) {
+
+    // for each potential shortcut
+    for (i = 0; i < _handlers[key].length; i++) {
       handler = _handlers[key][i];
+
+      // see if it's in the current scope
       if(handler.scope == _scope || handler.scope == 'all'){
-        modifiersMatch = (handler.mods.length > 0 && (function(){
-          for(hmi = 0, hmil = handler.mods.length; hmi < hmil; hmi++)
-            if(!_mods[handler.mods[hmi]])
-              return false;
-          return true;
-        })());
+        // check if modifiers match if any 
+        modifiersMatch = handler.mods.length > 0;
+        for(k in _mods)
+          if((!_mods[k] && handler.mods.indexOf(+k) > -1) ||
+            (_mods[k] && handler.mods.indexOf(+k) == -1)) modifiersMatch = false;    
+        // call the handler and stop the event if neccessary
         if((handler.mods.length == 0 && !_mods[16] && !_mods[18] && !_mods[17] && !_mods[91]) || modifiersMatch){
           if(handler.method(event, handler.key, handler.scope)===false){
-            if(event.stopPropagation) event.stopPropagation();
-            if(event.cancelBubble) event.cancelBubble();
             if(event.preventDefault) event.preventDefault();
               else event.returnValue = false;
+            if(event.stopPropagation) event.stopPropagation();
+            if(event.cancelBubble) event.cancelBubble = true;
           }
         }
       }
 	}
   };
 
+  // unset modifier keys on keyup
   function clearModifier(event){
     var key = event.keyCode, k;
+    if(key == 93 || key == 224) key = 91; 
     if(key in _mods) {
       _mods[key] = false;
       for(k in _MODIFIERS) if(_MODIFIERS[k] == key) assignKey[k] = false;
     }
   };
 
+  // parse and assign shortcut
   function assignKey(key, scope, method){
-    var keys, mods, i, mi, kl, ml;
+    var keys, mods, i, mi;
     if (method === undefined) {
       method = scope;
       scope = 'all';
     }
     key = key.replace(/\s/g,'');
     keys = key.split(',');
-    for (i = 0, kl = keys.length; i < kl; i++) {
+    // for each shortcut
+    for (i = 0; i < keys.length; i++) {
+      // set modifier keys if any
       mods = [];
       key = keys[i].split('+');
       if(key.length > 1){
         mods = key.slice(0,key.length-1);
-        for (mi = 0, ml = mods.length; mi < ml; mi++)
+        for (mi = 0; mi < mods.length; mi++)
           mods[mi] = _MODIFIERS[mods[mi]];
         key = [key[key.length-1]];
       }
+      // convert to keycode and...
       key = key[0]
       key = key.length > 1 ? _MAP[key] : key.toUpperCase().charCodeAt(0);
+      // ...store handler
       if (!(key in _handlers)) _handlers[key] = [];
       _handlers[key].push({ scope: scope, method: method, key: keys[i], mods: mods });
     }
   };
 
+  // initialize key.<modifier> to false
   for(k in _MODIFIERS) assignKey[k] = false;
 
+  // set current scope (default 'all')
   function setScope(scope){ _scope = scope || 'all' };
-  
+
+  // cross-browser events  
   function addEvent(object, event, method) {
     if (object.addEventListener)
       object.addEventListener(event, method, false);
@@ -94,9 +125,11 @@
       object.attachEvent('on'+event, function(){ method(window.event) });
   };
   
+  // set the handlers globally on document
   addEvent(document, 'keydown', dispatch);
   addEvent(document, 'keyup', clearModifier);
   
+  // set window.key and window.key.setScope
   global.key = assignKey;
   global.key.setScope = setScope;
 })(this);
