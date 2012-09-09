91daac4c25f4f8024bd16e6463f48f20398599c5
Zhang <clvrobj@gmail.com>
2012-09-09 11:59:03 +0800

    modifiers reset issue

--- keymaster.js
+++ keymaster.js
@@ -39,6 +39,16 @@
     return -1;
   }
 
+  var modifierMap = {
+      16:'shiftKey',
+      18:'altKey',
+      17:'ctrlKey',
+      91:'metaKey'
+  };
+  function updateModifierKey(event) {
+      for(k in _mods) _mods[k] = event[modifierMap[k]];
+  };
+
   // handle keydown event
   function dispatch(event, scope){
     var key, handler, k, i, modifiersMatch;
@@ -52,6 +62,7 @@
       for(k in _MODIFIERS) if(_MODIFIERS[k] == key) assignKey[k] = true;
       return;
     }
+    updateModifierKey(event);
 
     // see if we need to ignore the keypress (ftiler() can can be overridden)
     // by default ignore key presses if a select, textarea, or input is focused
