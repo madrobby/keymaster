d0e7a2815a3a0b9a8496d0e85fc0cea3b9cbcb21
Matt Enlow <matt@novaugust.net>
2014-06-20 08:45:28 -0600

    Add 'meta' to _MODIFIERS dictionary

--- keymaster.js
+++ keymaster.js
@@ -12,7 +12,7 @@
       '⇧': 16, shift: 16,
       '⌥': 18, alt: 18, option: 18,
       '⌃': 17, ctrl: 17, control: 17,
-      '⌘': 91, command: 91
+      '⌘': 91, command: 91, meta: 91
     },
     // special keys
     _MAP = {
