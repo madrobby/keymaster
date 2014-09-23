41545c5a9291fea096bb290d2aa3bb4c3bdf1696
Michael <mikach27@gmail.com>
2014-09-24 00:20:58 +0300

    Update keymaster.js

--- keymaster.js
+++ keymaster.js
@@ -264,8 +264,8 @@
   };
 
   // set the handlers globally on document
-  addEvent(document, 'keydown', function(event) { dispatch(event) }); // Passing _scope to a callback to ensure it remains the same by execution. Fixes #48
-  addEvent(document, 'keyup', clearModifier);
+  addEvent(window.document, 'keydown', function(event) { dispatch(event) }); // Passing _scope to a callback to ensure it remains the same by execution. Fixes #48
+  addEvent(window.document, 'keyup', clearModifier);
 
   // reset modifiers to false whenever the window is (re)focused.
   addEvent(window, 'focus', resetModifiers);
