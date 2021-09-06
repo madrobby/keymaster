3014a75b555488ce33cdce466e83b5b08891cc16
will <will@metawork.com>
2021-09-06 18:38:10 +0000

    guard document

--- keymaster.js
+++ keymaster.js
@@ -264,8 +264,10 @@
   };
 
   // set the handlers globally on document
-  addEvent(document, 'keydown', function(event) { dispatch(event) }); // Passing _scope to a callback to ensure it remains the same by execution. Fixes #48
-  addEvent(document, 'keyup', clearModifier);
+  if (document) {
+    addEvent(document, 'keydown', function(event) { dispatch(event) }); // Passing _scope to a callback to ensure it remains the same by execution. Fixes #48
+    addEvent(document, 'keyup', clearModifier);
+  }
 
   // reset modifiers to false whenever the window is (re)focused.
   addEvent(window, 'focus', resetModifiers);
